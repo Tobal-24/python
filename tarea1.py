b36e49bd2840e4ed914c816dcc7880720cf7af14
Tobal-24 <cri.abarcaa@duocuc.cl>
2025-04-09 09:41:32 -0400

    Add files via upload
    TAREA operaciones python

---
+++ tarea1.py
@@ -0,0 +1,67 @@
+#calculadora en orden
+
+
+
+V01 = int (input("dime tu numero entero para sumar: "))
+
+V02 = float (input ("dime tu numero decimal para sumar: "))
+
+
+# operación SUMA
+
+
+R01 = V01 + V02 
+
+
+#Salida
+
+print (F"El resultado es: {R01}")
+
+
+V01 = int (input("dime tu numero entero Para multiplicar:  "))
+
+V02 = float (input ("dime tu numero decimal para multiplicar: "))
+
+
+# operación Multiplicacion
+
+
+R01 = V01 * V02 
+
+
+#Salida
+
+print (F"El resultado es: {R01}")
+
+
+
+
+V01 = int (input("dime tu numero entero para resta:  "))
+
+V02 = float (input ("dime tu numero decimal para resta:  "))
+
+
+# operación RESTA
+
+
+R01 = V01 - V02 
+
+
+#Salida
+
+print (F"El resultado es: {R01}")
+
+V01 = int (input("dime tu numero entero para dividir:  "))
+
+V02 = float (input ("dime tu numero decimal para dividir: "))
+
+
+# operación DIVISIÓN
+
+
+R01 = V01 / V02 
+
+
+#Salida
+
+print (F"El resultado es: {R01}")
